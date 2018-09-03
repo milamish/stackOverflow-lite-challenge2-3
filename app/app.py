c71cac0a9e9a64d6f2489dc076d7d19688e8ef40
milamish <milamish@live.com>
2018-09-03 17:03:05 +0300

    ft:signup page #160232333

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import datetime
+
+app = Flask(__name__)
+api= Api(app)
+
+users = {}
+
+class Signup(Resource):
+    def post(self):
+        name= request.get_json()['name']
+        emailaddress= request.get_json()['emailaddress']
+        password= request.get_json()['password']
+        username=request.get_json()['username']
+        users.update({username:{"name": name,"emailadress": emailaddress,"password": password}})
+        return jsonify({"name": name},{"username":username})
+api.add_resource(Signup,'/api/v1/auth/signup' )
+
+class Login(Resource):
+    def post(self):
+        username=request.get_json()["username"]
+        password=request.get_json()["password"]
+        if username in users:
+            if password==users[username]["password"]:
+            	return jsonify({"message":"succesfuly logged in"})
+                        
+            else:
+                return jsonify({"message": "your password is wrong"})
+        else:
+            return jsonify({"message": "check your username"})
+api.add_resource(Login,'/api/v1/auth/login')
