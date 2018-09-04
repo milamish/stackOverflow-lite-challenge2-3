3a25c49580b714862f8cbbcf9d32377dc16b5d05
milamish <milamish@live.com>
2018-09-04 14:40:48 +0300

    ft: fix sign up

--- app/app.py
+++ app/app.py
@@ -16,8 +16,11 @@ class Signup(Resource):
         emailaddress= request.get_json()['emailaddress']
         password= request.get_json()['password']
         username=request.get_json()['username']
-        users.update({username:{"name": name,"emailadress": emailaddress,"password": password}})
-        return jsonify({"name": name},{"username":username})
+        if username in users:
+            return jsonify({"message":"user already exists"}), 409
+        else:
+            users.update({username:{"name": name,"emailadress": emailaddress,"password": password}})
+            return jsonify({"name": name},{"username":username})
 api.add_resource(Signup,'/stackoverflowlite.com/api/v1/auth/signup' )
 
 class Login(Resource):
