73bd1ba21585d0e5d78a9797b1bae83d39dc8dbc
milamish <milamish@live.com>
2018-09-05 13:36:35 +0300

    fix pep8 issue

--- app/app.py
+++ app/app.py
@@ -1,21 +1,24 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_restful import Api
+from flask_restful import Resource
 
 app = Flask(__name__)
 api= Api(app)
 
 users = {}
-query=[]
-answer=[]
-answers={"answer":answer}
-queries={"query":query}
+query = []
+answer = []
+answers = {"answer":answer}
+queries = {"query":query}
 
 class Signup(Resource):
     def post(self):
-        name= request.get_json()['name']
-        emailaddress= request.get_json()['emailaddress']
-        password= request.get_json()['password']
-        username=request.get_json()['username']
+        name = request.get_json()['name']
+        emailaddress = request.get_json()['emailaddress']
+        password = request.get_json()['password']
+        username = request.get_json()['username']
         if username in users:
             return jsonify({"message":"user already exists"}), 409
         else:
@@ -25,10 +28,10 @@ api.add_resource(Signup,'/stackoverflowlite.com/api/v1/auth/signup' )
 
 class Login(Resource):
     def post(self):
-        username=request.get_json()["username"]
-        password=request.get_json()["password"]
+        username = request.get_json()["username"]
+        password = request.get_json()["password"]
         if username in users:
-            if password==users[username]["password"]:
+            if password == users[username]["password"]:
             	return jsonify({"message":"succesfuly logged in"})           
             else:
                 return jsonify({"message": "your password is wrong"})
@@ -36,19 +39,19 @@ class Login(Resource):
             return jsonify({"message": "check your username"})
 api.add_resource(Login,'/stackoverflowlite.com/api/v1/auth/login')
 
-class PostQuestion(Resource):
+class Post_question(Resource):
     def post(self):
-        question=request.get_json()["question"]
+        question = request.get_json()["question"]
         query.append({"question":question})
         return jsonify({"question":question})
-api.add_resource(PostQuestion,'/stackoverflowlite.com/api/v1/question')
+api.add_resource(Post_question,'/stackoverflowlite.com/api/v1/question')
 
 class Answer(Resource):
 	def post(self,ID):
-		post_answer= request.get_json()['post_answer']
+		post_answer = request.get_json()['post_answer']
 		try:
 			if ID in query:
-				if ID!=query[question]['ID']:
+				if ID!= query[question]['ID']:
 					return jsonify({"message":"question does not exist"})
 				else:
 					return jsonify({"message":"unable to post answer"}), 500
@@ -60,7 +63,7 @@ class Answer(Resource):
 api.add_resource(Answer,'/stackoverflowlite.com/api/v1/question/<int:ID>/answer')
 
 
-class DeleteQuestion(Resource):
+class Delete_question(Resource):
 	def delete(self, ID):
 		try:
 			if ID in query is None:
@@ -70,9 +73,9 @@ class DeleteQuestion(Resource):
 				return jsonify({"message":"question succesfuly deleted"})
 		except:
 			return jsonify({"message":"question ID does not exist"})
-api.add_resource(DeleteQuestion, '/stackoverflowlite.com/api/v1/question/<int:ID>')
+api.add_resource(Delete_question, '/stackoverflowlite.com/api/v1/question/<int:ID>')
 
-class GetQuestions(Resource):
+class Get_questions(Resource):
 	def get(self):
 		try:
 			if queries is None:
@@ -81,9 +84,9 @@ class GetQuestions(Resource):
 				return jsonify(queries)
 		except:
 			return jsonify({"message":"unable to fetch questions"})
-api.add_resource(GetQuestions, '/stackoverflowlite.com/api/v1/question')
+api.add_resource(Get_questions, '/stackoverflowlite.com/api/v1/question')
 
-class getOneQuestion(Resource):
+class Get_one_question(Resource):
 	def get(self, ID):
 		try:
 			if ID in query is None:
@@ -93,14 +96,14 @@ class getOneQuestion(Resource):
 		except:
 			return jsonify({"message":"question ID does not exist"})
 			
-api.add_resource(getOneQuestion, '/stackoverflowlite.com/api/v1/question/<int:ID>')
+api.add_resource(Get_one_question, '/stackoverflowlite.com/api/v1/question/<int:ID>')
 
-class getAnswers(Resource):
+class Get_answers(Resource):
 	def get(self):
 		return jsonify(answers)
-api.add_resource(getAnswers, '/stackoverflowlite.com/api/v1/answers')
+api.add_resource(Get_answers, '/stackoverflowlite.com/api/v1/answers')
 
-class UpdateAnswer(Resource):
+class Update_answer(Resource):
 	def put(self, ID):
 		update_answer = request.get_json()['update_answer']
 		try:
@@ -111,7 +114,7 @@ class UpdateAnswer(Resource):
 				return jsonify({"update_answer":update_answer, "answer":answer[ID-1]})
 		except:
 				return jsonify({"message":"Answer ID does not exist"})
-api.add_resource(UpdateAnswer, '/stackoverflowlite.com/api/v1/answer/<int:ID>')
+api.add_resource(Update_answer, '/stackoverflowlite.com/api/v1/answer/<int:ID>')
 
 if __name__=="__main__":
 	app.run(debug=True)
