de1111703a92a2862b9302238ce68d1bea1a7537
milamish <milamish@live.com>
2018-09-09 21:26:57 +0300

    ft: get a question #160369711

--- app/queries/views.py
+++ app/queries/views.py
@@ -46,5 +46,33 @@ class PostQuestion(Resource):
 			return{"message":"unable to post a question"}, 500
 		connection.commit()
 		return {"title":title,"question":question,"user_id":user_id}, 200
-
 api.add_resource(PostQuestion, '/api/v1/question')
+
+#this class allows users to get a single question using the question ID
+class GetQuestion(Resource):
+	@tokens
+	def get(self, question_id):
+		data = jwt.decode(request.headers.get('x-access-token'), app.config['SECRET_KEY'])
+		user_id=data['user_id']
+
+		try:
+			
+				get_question(question_id)
+				try:
+					get_question(question_id)
+					result=cursor.fetchone()
+					if result is None:
+						return {"message":"question_id does not exist"}, 404
+					else:
+						user_id=result[4]
+						title = result[1]
+						question=result[2]
+						question_date= result[3]
+						question_id=result[0]
+						return jsonify({"user_id":user_id,"title":title, "question":question, "question_date":question_date, "question_id":question_id})
+				except:
+					return{"message": "unable to fetch entry"}, 500
+				connection.commit()
+		finally:
+			pass
+api.add_resource(GetQuestion, '/api/v1/question/<int:question_id>')
